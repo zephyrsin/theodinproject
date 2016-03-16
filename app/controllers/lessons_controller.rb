5c28e0bef352c4bd75ce48b8efb8b80c1e631d19
Mazin Power <mazin.power@icloud.com>
2016-03-16 08:20:17 +0000

    Start work on progress tracking (SPIKE)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -25,12 +25,19 @@ class LessonsController < ApplicationController
 
     raise ActionController::RoutingError.new('Not Found') unless @course.is_active
 
-    
     @content = md(@lesson.content) if @lesson.content
     @next_lesson = @lesson.next_lesson
     @prev_lesson = @lesson.prev_lesson
     @num_lessons = @lesson.section.lessons.where(:is_project => false).count
     @num_projects = @lesson.section.lessons.where(:is_project => true).count
+
+    #SPIKE! These two queries run the logic we want - but are by no means final.
+    #TODO: Write tests, rewrite this crap :)
+    @completion_ids = @lesson.lesson_completion_ids
+    @current_students = @prev_lesson.lesson_completion_ids.find_all do |completion_id|
+      User.find(completion_id).send(:latest_completed_lesson).id == @prev_lesson.id
+    end
+
     # the position of the lesson not including projects
     @lesson_position_in_section = @lesson.section.lessons.where("is_project = ? AND position <= ?", false, @lesson.position).count
     # puts "\n\nVE BANNER #{Ad.show_ads?(current_user)}!\n\n"
